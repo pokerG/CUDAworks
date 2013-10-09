fb42a050f6844819b1da563367a2da524c8fdd8d
Leiguang Hao <pokerfacehlg@gmail.com>
2013-10-09 17:10:39 +0800

    task1

--- task1/task1-2.cu
+++ task1/task1-2.cu
@@ -7,7 +7,6 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void addKernel(int *c, const int *a, const int *b){
 	int i = threadIdx.x;
-	printf("thread %d",i);
 	c[i] = a[i] + b[i];
 }
 
