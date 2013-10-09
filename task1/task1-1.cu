8499a165d986d61064cc0ab07e85f99b52c1000b
Leiguang Hao <pokerfacehlg@gmail.com>
2013-10-09 17:09:41 +0800

    task1

--- task1/task1-1.cu
+++ task1/task1-1.cu
@@ -2,29 +2,7 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "string.h"
-/*bool InitCUDA(){
-	int count;
-	cudaGetDeviceCount(&count);
-	if(count == 0){
-		fprintf(stderr, "There is no device.]n");
-		return false;
-	}
-	int i;
-	for(i = 0; i < count; i++){
-		cudaDeviceProp prop;
-		if(cudaGetDeviceProperties(&prop,i) == cudaSuccess){
-			if(prop.major >= 1){
-				break;
-			}
-		}
-	}
-	if(i == count){
-		fprintf(stderr,"There is no device suppoting CUDA 1.x.\n");
-		return false;
-	}
-	cudaSetDevice(i);
-	return true;
-}*/
+
 void printDevice(cudaDeviceProp prop){
 	printf("\t Name: \t%s\n",prop.name);
     printf("\t Capability Major/Minor version number:    %d.%d\n", prop.major, prop.minor);
--- task1/task1-2.cu
+++ task1/task1-2.cu
@@ -5,8 +5,9 @@
 #include <stdlib.h>
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
-__global__ void  addKernel(int *c, const int *a, const int *b){
+__global__ void addKernel(int *c, const int *a, const int *b){
 	int i = threadIdx.x;
+	printf("thread %d",i);
 	c[i] = a[i] + b[i];
 }
 
